4dfbb6526299e92f14f2385608b4e26c4862c950
Hamdi Ahmed <a.hamdi@prosylab.com>
2023-10-10 17:15:22 +0300

    feat:Apply sort functionality on orders list

--- src/components/orders/useOrderList.ts
+++ src/components/orders/useOrderList.ts
@@ -1,9 +1,38 @@
+import { ordersList } from "@/types/orders";
+import { useRef, useState } from "react";
 
 
 const useOrderList = () => {
+    // ** states
+    const [orders, setOrders] = useState<ordersList[]>([])
 
+    // ** refs
+    const dragOrder = useRef<any>(null);
+    const dragOverOrder = useRef<any>(null);
+
+    // ** drag && sort
+    const handleSort = () => {
+        //** copy items
+        let ordersItems = [...orders];
+
+        const draggedItemContent = ordersItems.splice(dragOrder.current, 1)[0];
+
+        // ** replace the index
+        ordersItems.splice(dragOverOrder.current, 0, draggedItemContent);
+
+        dragOrder.current = null;
+        dragOverOrder.current = null;
+
+        // ** update order list
+        setOrders(ordersItems);
+    };
 
     return {
+        orders,
+        setOrders,
+        handleSort,
+        dragOrder,
+        dragOverOrder
     }
 }
 
